dc7ffcb5d0bfea4960127d3ad52b1b0c8fc880a0
Luke Kozakewycz <lukeoncloud@gmail.com>
2021-01-12 21:38:40 +0000

    Remove component definition from rerender cycle

--- src/components/common/icons/logo_icon.tsx
+++ src/components/common/icons/logo_icon.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import ReactSVG from 'react-svg';
 import styled, { withTheme , DefaultTheme} from 'styled-components';
-
-import { Theme } from '../../../themes/commons';
 
 interface Props {
     isInline?: boolean;
@@ -20,33 +17,24 @@ const IconContainer = styled.div<{ color: string; isInline?: boolean; width?: st
     width: ${props => (props.width || '200px')};
 `;
 
+const IconImage = styled.img`
+    width: 80%;
+    display: flex;
+`;
+
 const LogoIconContainer = (props: Props) => {
     const { theme, icon, ...restProps } = props;
 
-    const fallBack = null;
     const isSvg = new RegExp('.svg$');
     const isImage = new RegExp('(http(s?):)?.*.(?:jpg|gif|png)');
 
-    let Icon;
-    if (isSvg.test(icon as string)) {
-        Icon = styled.img`
-            width: 80%;
-            display: flex;
-        `;
-    }
-    if (isImage.test(icon as string)) {
-        Icon = styled.img`
-            width: 80%;
-            display: flex;
-        `;
-    }
-    if (!Icon) {
+    if (!isSvg.test(icon as string) && !isImage.test(icon as string)) {
         return null;
     }
 
     return (
         <IconContainer color="transparent" {...restProps}>
-            <Icon src={icon as string} alt="logo"></Icon>
+            <IconImage src={icon as string} alt="logo" />
         </IconContainer>
     );
 };
